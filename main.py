77237ecda4d4f979f26fc189c942aa829aec9736
MuhammadGalimov <kakasi.muham@gmail.com>
2020-10-29 12:16:22 +0500

    added numbers

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import consts
 class Cell(pygame.sprite.Sprite):
     def __init__(self, spr, center, isBombed):
         pygame.sprite.Sprite.__init__(self)
+        self.center = center
         self.image = pygame.image.load(spr).convert_alpha()
-        self.rect = self.image.get_rect(center=center)
+        self.rect = self.image.get_rect(center=self.center)
 
         self.states = {'cell': 1, 'empty': 0, 'flag': 0}
         self.isBombed = isBombed
 
-    def click_empty(self):
-        self.states.update({'cell': 0, 'empty': 1, 'flag': 0})
-        self.image = pygame.image.load(consts.spr_empty).convert_alpha()
+    def click_left(self, n):
+        string = consts.spr_i
+        string = string.replace('I', str(n))
 
-    def click_flag(self):
+        self.image = pygame.image.load(string).convert_alpha()
+        self.states.update({'cell': 0, 'empty': 1, 'flag': 0})
+
+    def click_right(self):
         self.states.update({'cell': 0, 'empty': 0, 'flag': 1})
         self.image = pygame.image.load(consts.spr_flag).convert_alpha()
 
@@ -32,10 +36,9 @@ clock = pygame.time.Clock()
 
 board = Board(consts.spr_cell, 10)
 
-board.board[1][1].isFlagged = True
-board.board[5][5].isOpened = True
-board.board[1][1].update()
-board.board[5][5].update()
+board.board[1][1].click_left(5)
+board.board[1][7].click_left(7)
+board.board[3][3].click_right()
 
 pygame.display.update()
 
